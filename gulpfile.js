3ad44afd7bd25a6700ea1921502c2385298d2d40
Vika <your_email@whatever.com>
2021-12-07 15:07:16 +0200

    2h

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,56 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const watch = require('gulp-watch');
+let project_folder = "css";
+let sourse_folder = "src";
 
-gulp.task('sass-compile', function (){
-  return gulp.src('./scss/**/*.scss', ['sass'])
-      .pipe(sourcemaps.init())
-      .pipe(sass().on('error', sass.logError))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./css/'));
-})
+let path = {
+  build: {
+    css: project_folder + "/",
+  },
+  src: {
+    css: sourse_folder + "/scss/style.scss",
+  },
+  watch: {
+    css: sourse_folder + "/scss/**/*.scss",
+  },
+  clean: "./" + project_folder + "/",
+};
 
-gulp.task('watch', function (){
-  gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'))
-})
+let { src, dest } = require("gulp"),
+  gulp = require("gulp"),
+  del = require("del"),
+  scss = require("gulp-sass")(require("sass")),
+  autoprefixer = require("gulp-autoprefixer"),
+  gcmq = require("gulp-group-css-media-queries");
+
+function css() {
+  return gulp
+    .src(path.src.css)
+    .pipe(
+      scss({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 5 version"],
+        cascade: true,
+      })
+    )
+    .pipe(gcmq())
+    .pipe(dest(path.build.css));
+}
+
+function watchFiles() {
+  gulp.watch([path.watch.css], css);
+}
+
+function clean() {
+  return del(path.clean);
+}
+
+let build = gulp.series(clean, css);
+let watch = gulp.parallel(build, watchFiles);
+
+exports.css = css;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,3 @@
-
-// core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from 'swiper';
-// import Swiper and modules styles
-
-
 // configure Swiper to use modules
 const swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
